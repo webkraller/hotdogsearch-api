12160120de261f2846536ab3a2bed32cdcbdd18f
Alex Krall <alex@compound.re>
2020-09-03 13:27:04 -0400

    removed and changed key from git... whoops

--- hotdogapi/settings.py
+++ hotdogapi/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'qk-$jm=9k#jh&kyp0kb1om*k-dd^dia(ejaf&w9lbpzl5em5so'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -120,3 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+keypath = BASE_DIR / 'secret_key.key'
+with open(keypath) as f:
+    SECRET_KEY = f.read().strip()
